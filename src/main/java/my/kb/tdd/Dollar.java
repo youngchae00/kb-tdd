25d9f53ad2c2c4088f2f79eee3e35e7301237268
youngchae00 <45rrrr@gmail.com>
2023-05-31 22:36:16 +0900

    chap01 Money Test

---
+++ src/main/java/my/kb/tdd/Dollar.java
@@ -0,0 +1,14 @@
+package my.kb.tdd;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount){
+        this.amount = amount;
+    }
+
+    public void times(int multiplier){
+        amount *= multiplier;
+    }
+
+}
